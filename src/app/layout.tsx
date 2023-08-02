e1387bfca25bcb978559ab166a3b09f7f676a066
jed peek <jedpeek@MacBook-Pro-2.lan>
2023-08-02 16:01:00 -0700

    updated tsx extensions in page and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "../components/header.tsx";
-import Footer from "../components/footer.tsx";
-import Hero from "../components/hero.tsx";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Hero from "../components/hero";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "../components/header";
-import Hero from "../components/hero.tsx";
+import Hero from "../components/hero";
 import Contact from "../components/contact";
 import Content from "../components/content";
 export default function Home() {

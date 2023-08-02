f1092438df8855c2a552216be273d036d7cde066
jed peek <jedpeek@MacBook-Pro-2.lan>
2023-08-02 16:03:45 -0700

    updated event in send email function

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,7 +4,7 @@ import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef();
-  const sendEmail = (e) => {
+  const sendEmail = (e: React.FormEvent<HTMLInputElement>) => {
     e.preventDefault();
 
     emailjs

b956f8af823644b23d62978754574627b088c334
jed peek <jedpeek@MacBook-Pro-2.lan>
2023-08-02 17:11:21 -0700

    updated contact button in hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -18,9 +18,11 @@ const Hero = () => {
             resonate with the hearts of both the couple and their guests.
           </p>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              CONTACT
-            </button>
+            <Link href="#contact">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                CONTACT
+              </button>
+            </Link>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">

5aa65c1dc04dc9793d5e88a1ab1eb303e764aa7c
jed peek <jedpeek@MacBook-Pro-2.localdomain>
2023-11-05 08:54:01 -0800

    update band instagram

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           <a
-            href="https://www.instagram.com/fifthfloor.band/"
+            href="https://www.instagram.com/fifthfloorsd"
             className="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
